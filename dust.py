da65b36e8b6e86500f54ff03e6b5f564783d3f6c
mcthoren <thoren.mcdole@gmail.com>
2018-10-27 14:27:06 +0000

    functionerize, taberize, add file write out functions

--- dust.py
+++ dust.py
@@ -1,31 +1,51 @@
 #!/usr/local/bin/python2.7
 # -*- coding: UTF-8 -*-
+# this code indented with actual 0x09 tabs
 
-#https://gist.github.com/geoffwatts/b0b488b5a5257223ed53
+# This is a hacked up version of code from: https://gist.github.com/geoffwatts/b0b488b5a5257223ed53
 
 import serial, time, datetime, struct
+wx_dir = "/home/ghz/dust"
 
-ser = serial.Serial()
-ser.port = "/dev/ttyU0" # Set this to your serial port
-ser.baudrate = 9600
+def write_out(file_name, data, mode):
+	out_file_fd = open(file_name, mode)
+	out_file_fd.write(data)
+	out_file_fd.close()
 
-ser.open()
-ser.flushInput()
+def write_out_dat_stamp(ts, n_plate, data):
+	# year directories should be created once a year from cron
+	# that way we aren't unnecessarily checking for one every minute of every day for a year
 
-byte, lastbyte = "\x00", "\x00"
+	f_ts = ts[0:8]
+	y_ts = ts[0:4]
+	write_out(wx_dir+'/data/'+y_ts+'/'+n_plate+'.'+f_ts, data, 'a')
 
-while True:
-    lastbyte = byte
-    byte = ser.read(size=1)
+if __name__ == "__main__":
+
+	dat_fname = 'dust.dat'
+
+	ser = serial.Serial()
+	ser.port = "/dev/ttyU0" # Set this to your serial port
+	ser.baudrate = 9600
+
+	ser.open()
+	ser.flushInput()
+
+	byte, lastbyte = "\x00", "\x00"
+
+	while True:
+		lastbyte = byte
+		byte = ser.read(size=1)
     
-    # We got a valid packet header
-    if lastbyte == "\xAA" and byte == "\xC0":
-        sentence = ser.read(size=8) # Read 8 more bytes
-        readings = struct.unpack('<hhxxcc',sentence) # Decode the packet - big endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
+		# We got a valid packet header
+		if lastbyte == "\xAA" and byte == "\xC0":
+			sentence = ser.read(size=8) # Read 8 more bytes
+			readings = struct.unpack('<hhxxcc',sentence) # Decode the packet - big endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
         
-        pm_25 = readings[0]/10.0
-        pm_10 = readings[1]/10.0
-        # ignoring the checksum and message tail
+			pm_25 = readings[0]/10.0
+			pm_10 = readings[1]/10.0
+			# ignoring the checksum and message tail
         
-	ts =  datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-        print ts,"\tPM 2.5:",pm_25,"μg/m³  PM 10:",pm_10,"μg/m³"
+			ts =  datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+			dat_string "%s\tPM 2.5: %3.3f μg/m³\tPM 10: %3.3f μg/m³" % (ts, pm_25, pm10)
+			write_out_dat_stamp(ts, dat_fname, dat_string)
