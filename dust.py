1bcb77741c9660d7c6a2b5c25204fe51e5df6d97
mcthoren <thoren.mcdole@gmail.com>
2018-10-28 09:06:33 +0000

    correct comment to reflect actual little endian nature

--- dust.py
+++ dust.py
@@ -33,6 +33,8 @@ if __name__ == "__main__":
 
 	byte, lastbyte = "\x00", "\x00"
 
+	time0 = time1 = time.time()
+
 	while True:
 		lastbyte = byte
 		byte = ser.read(size=1)
@@ -40,7 +42,9 @@ if __name__ == "__main__":
 		# We got a valid packet header
 		if lastbyte == "\xAA" and byte == "\xC0":
 			sentence = ser.read(size=8) # Read 8 more bytes
-			readings = struct.unpack('<hhxxcc',sentence) # Decode the packet - big endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
+
+			# Decode the packet - little endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
+			readings = struct.unpack('<hhxxcc',sentence)
         
 			pm_25 = readings[0]/10.0
 			pm_10 = readings[1]/10.0
