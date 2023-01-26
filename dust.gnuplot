729b0fb6882d5f52f92de396d674c999e7a200d3
mcthoren <thoren.mcdole@gmail.com>
2023-01-26 18:36:57 +0000

    remove python plotting. move over to existing gnuplot plotting. unify code between the multiple installs more.

--- dust.gnuplot
+++ dust.gnuplot
@@ -16,18 +16,20 @@ set term pngcairo size 2000, 512 font ",10"
 set format y "%.1f"
 set format y2 "%.1f"
 
-dat_f="/import/home/ghz/repos/dust_wx/data/dust.dat.2-3_day"
+HOME=system('echo "${HOME}"')
+
+dat_f=HOME."/repos/dust_wx/data/dust.dat.2-3_day"
 
 set ylabel "PM_{2.5} (µg/m³)"
 set y2label "PM_{2.5} (µg/m³)"
-set output "/import/home/ghz/repos/dust_wx/plots/pm_25.png"
+set output HOME."/repos/dust_wx/plots/pm_25.png"
 plot 5 title 'WHO Annual PM_{2.5} Guidline: 5 µg/m³' with lines lw 1 linecolor rgb "#c00000", \
 15 title 'WHO 24-Hour PM_{2.5} Guidline: 15 µg/m³' with lines lw 1 linecolor rgb "#bb00ff", \
 dat_f using 1:4 title 'Particulate Matter' with lines lw 2 linecolor rgb "#0000dd"
 
 set ylabel "PM_{10} (µg/m³)"
 set y2label "PM_{10} (µg/m³)"
-set output "/import/home/ghz/repos/dust_wx/plots/pm_10.png"
+set output HOME."/repos/dust_wx/plots/pm_10.png"
 plot 15 title 'WHO Annual PM_{10} Guidline: 15 µg/m³' with lines lw 1 linecolor rgb "#c00000", \
 45 title 'WHO 24-Hour PM_{10} Guidline: 45 µg/m³' with lines lw 1 linecolor rgb "#bb00ff", \
 dat_f using 1:8 title 'Particulate Matter' with lines lw 2 linecolor rgb "#00dd00"
--- dust.py
+++ dust.py
@@ -4,7 +4,7 @@
 
 # This is a hacked up version of code from: https://gist.github.com/geoffwatts/b0b488b5a5257223ed53
 
-import sys, serial, time, datetime, struct, fileinput, os
+import sys, serial, time, datetime, struct
 import numpy as np
 import matplotlib.dates as mdates
 
@@ -13,34 +13,6 @@ import wxlib as wx
 
 wx_dir = "/home/ghz/dust"
 plot_d = wx_dir+'/plots/'
-
-def plot(ts, n_plate):
-	npoints = 3000 # ~48h
-
-	dat_f = ["1000", "0100", "0010", "0001"]
-
-	td = datetime.datetime.strptime(ts, "%Y%m%d%H%M%S")
-
-	for i in range(0, 4):
-		d_date = (td - datetime.timedelta(i)).strftime("%Y%m%d")
-		d_year = (td - datetime.timedelta(i)).strftime("%Y")
-		dat_f[3 - i] = wx_dir+'/data/'+d_year+'/'+n_plate+'.'+d_date
-		wx.proof_dat_f(dat_f[3 - i])
-
-	dust_dat  = fileinput.input(dat_f)
-	date, pm25, pm10 = np.loadtxt(dust_dat, usecols=(0, 3, 7), unpack=True, encoding = u'utf8', converters={ 0: mdates.strpdate2num('%Y%m%d%H%M%S')})
-
-	if date.size < 4:
-		return 0; # not enough points yet. wait for more
-
-	if date.size < npoints:
-		npoints = date.size - 1
-
-	f_pts  = date.size - npoints
-	t_pts  = date.size
-
-	wx.graph(date[f_pts : t_pts], pm25[f_pts : t_pts], "b-", "Particulate Matter", r'$PM_{2.5}\ (\mu g/m^3)$', plot_d+'dust_pm25.png')
-	wx.graph(date[f_pts : t_pts], pm10[f_pts : t_pts], "g-", "Particulate Matter", r'$PM_{10}\ (\mu g/m^3)$', plot_d+'dust_pm10.png')
 
 def gen_index(pm25, pm10):
 	plate = wx_dir+"/dust_wx_index.html.template"
@@ -105,13 +77,12 @@ if __name__ == "__main__":
 			count += 1
 			time1 = time.time()
 			if((time1 - time0) > 60):
-				ts = time.strftime("%Y%m%d%H%M%S", time.gmtime(float(time1)))
+				ts = time.strftime("%FT%T%Z", time.gmtime(float(time1)))
+				tsf = time.strftime("%Y%m%d%H%M%S", time.gmtime(float(time1)))
 				pm_25 = pm_25_val / count / 10.0
 				pm_10 = pm_10_val / count / 10.0
 				dat_string = "%s\tPM 2.5: %.2f μg/m³\tPM 10: %.2f μg/m³\n" % (ts, pm_25, pm_10)
-				wx.write_out_dat_stamp(ts, dat_fname, dat_string, wx_dir)
-				plot(ts, dat_fname)
+				wx.write_out_dat_stamp(tsf, dat_fname, dat_string, wx_dir)
 				gen_index(pm_25, pm_10)
-				os.system("/usr/bin/rsync -ur --timeout=55 /home/ghz/dust/* wx3@wx3.slackology.net:/wx3/")
 				pm_25_val = pm_10_val = count = 0
 				time0 = time1 = time.time()
